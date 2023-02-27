c5a4074edc534fbbddf6bdc61c0997b6d0803a0f
Firas Gharsellaoui <firas.gharsellaoui@rhis-solutions.com>
2023-02-27 10:15:14 +0100

    migrate to .net 7

--- supermarketapp/Startup.cs
+++ supermarketapp/Startup.cs
@@ -2,6 +2,7 @@
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
 
 namespace supermarketapp
 {
@@ -14,7 +15,7 @@ namespace supermarketapp
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        public void Configure(IApplicationBuilder app, IHostEnvironment env)
         {
             if (env.IsDevelopment())
             {
